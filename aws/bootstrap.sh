0e4c1ee785dd2822f8e432e8fe0d08041f5a37d5
migmoren <Miguel.Moreno.-ND@disney.com>
2025-09-30 20:08:03 -0600

    bootstrap updated to use: git + clone app repo + Flask + python3 + python venv

--- aws/bootstrap.sh
+++ aws/bootstrap.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# Update package lists (Debian/Ubuntu)
+# Log all script output to a file for easier debugging
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
 sudo apt-get update -y
+sudo apt-get install -y python3 python3-pip python3-venv git
 
-# Install Python3
-sudo apt-get install -y python3 python3-pip
+sudo mkdir -p /opt/mikeapp
+sudo chown -R ubuntu:ubuntu /opt/mikeapp
 
-# Install pipx for isolated Python package management
-sudo apt-get install -y pipx
-pipx ensurepath
+# Clone the app from its repository
+sudo -H -u ubuntu git clone https://github.com/migmoren/mikeapp.git /opt/mikeapp
 
-# Activate pipx environment
-source /home/ubuntu/.local/share/pipx/venvs/flask/bin/activate
+# Create a Python virtual environment
+cd /opt/mikeapp
+sudo -H -u ubuntu python3 -m venv venv
 
-# Install Flask using pipx
-pipx install flask
+# Install flask
+sudo -H -u ubuntu /opt/mikeapp/venv/bin/pip install Flask
 
-# Start a sample Flask app
-python3 app.py
+# Start the Flask app in the background and log app's output to a file
+cd /opt/mikeapp/src
+sudo -H -u ubuntu nohup /opt/mikeapp/venv/bin/python3 app.py > /opt/mikeapp/app.log 2>&1 &
